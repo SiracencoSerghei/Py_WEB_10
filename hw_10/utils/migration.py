a32d76dbe236c328da1f05cfcc80ab4efddc0e68
Serghei <siracencoserghei@gmail.com>
2024-03-11 14:53:05 +0100

    quotes migration

--- hw_10/utils/migration.py
+++ hw_10/utils/migration.py
@@ -30,26 +30,4 @@ def import_records():
 
         Author.objects.get_or_create(**author_data)
 
-    quotes = db.quotes.find()
-    for quote in quotes:
-        tags = []
-        for tag in quote.get("tags"):
-            if tag:
-                t, *_ = Tag.objects.get_or_create(name=tag)
-                tags.append(t)
-
-        exist_quote = Quote.objects.filter(text=quote["quote"]).exists()
-        if not exist_quote:
-            author = db.authors.find_one({"_id": quote["author"]})
-            a = Author.objects.get(fullname=author["fullname"])
-            quote_data = {
-                "author": a,
-                "text": quote["quote"],
-            }  # Use 'text' instead of 'quote'
-            q = Quote.objects.create(**quote_data)
-            for tag in tags:
-                q.tags.add(tag)
-
-
-if __name__ == "__main__":
-    import_records()
+   
