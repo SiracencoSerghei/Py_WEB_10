32938f711b9b17786e7985aa22986055cc3274d8
Serghei <siracencoserghei@gmail.com>
2024-03-11 15:32:23 +0100

    migration for quotes

--- hw_10/utils/migration.py
+++ hw_10/utils/migration.py
@@ -19,6 +19,7 @@ from quotes.utils import get_mongodb
 def import_records():
     db = get_mongodb()
     authors = db.authors.find()
+    quotes = db.quotes.find()
 
     for author in authors:
         author_data = {
@@ -29,5 +30,25 @@ def import_records():
         }
 
         Author.objects.get_or_create(**author_data)
+    for  quote in quotes:
+        # print(quote['tags'])
+        tags = []
+        for tag in quote['tags']:
+            t, *_ = Tag.objects.get_or_create(name=tag)
+            tags.append(t)
+        exist_quote = bool(len(Quote.objects.filter(quote =  quote['quote'])))
+        
+        if not exist_quote:
+            author = db.authors.find_one({'_id': quote['author']})
+            a = Author.objects.get(fullname = author['fullname'])
+            q = Quote.objects.create(
+                quote=quote.get("quote"),
+                author = a
+            )
+            for tag in tags:
+                q.tags.add(tags)
+                
 
-   
+if  __name__ == '__main__':
+    import_records()
+    
