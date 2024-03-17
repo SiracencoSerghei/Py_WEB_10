1703643480f8ca48b11812d33501cd1d74cee7a5
Serghei <siracencoserghei@gmail.com>
2024-03-17 12:55:31 +0100

    Allow all hosts

--- hw_10/hw_10/settings.py
+++ hw_10/hw_10/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Allow all hosts
 
 
 # Application definition
